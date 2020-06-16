5a9888e72dfaed65e9b6aea65def2e775c8d85bc
yan <yqp_001@163.com>
2020-06-16 18:13:35 +0800

    要用最简化的测试！

--- server.py
+++ server.py
@@ -2,23 +2,13 @@
 import os
 
 from flask import Flask
-from pymongo import MongoClient
-from redis import Redis
 
 app = Flask(__name__)
-
-client = MongoClient("mongo:27017")
-redis = Redis(host='redis', port=6379)
 
 
 @app.route('/')
 def todo():
-    try:
-        client.admin.command('ismaster')
-    except:
-        return "Server not available"
-
-    return f"Hello from the MongoDB & Redis ! \n Hits: {redis.incr('hits')}"
+    return "Hello Flask !"
 
 
 if __name__ == "__main__":
