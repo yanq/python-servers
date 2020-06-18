b27f81fc91a103f53efb14c5e098e875425aa73a
yan <yqp_001@163.com>
2020-06-18 18:32:50 +0800

    增加了 redis。恢复到最初复制的版本。

--- server.py
+++ server.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from pymongo import MongoClient
+from redis import Redis
 
 app = Flask(__name__)
+
+client = MongoClient("mongo:27017")
+redis = Redis(host='redis', port=6379)
 
 
 @app.route('/')
 def todo():
-    return "Hello Python!"
+    try:
+        client.admin.command('ismaster')
+    except:
+        return "Server not available"
+
+    return f"Hello from the MongoDB & Redis ! \n Hits: {redis.incr('hits')}"
 
 
 if __name__ == "__main__":
