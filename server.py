17b2145ab784761dab0eaae0d8fffffa3f53787a
yan <yqp_001@163.com>
2020-06-23 20:25:27 +0800

    测试安全 cookie。了解了。

--- server.py
+++ server.py
@@ -5,11 +5,13 @@ from tornado.options import options
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        if not self.get_secure_cookie('name'):
+            self.set_secure_cookie('name', 'yan')
         self.write("Hello Tornado !")
 
 
 if __name__ == "__main__":
     options.parse_command_line()
-    app = tornado.web.Application([(r"/", MainHandler), ], debug=True)
+    app = tornado.web.Application([(r"/", MainHandler), ], debug=True, cookie_secret="ANBHLLJKJIOLKJLKJLKJHGIU(OJBG")
     app.listen(8080)
     tornado.ioloop.IOLoop.current().start()
