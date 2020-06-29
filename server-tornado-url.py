e0a2fbf913bff7de12ca6c1164e859ad6507fc72
yan <yqp_001@163.com>
2020-06-29 14:58:28 +0800

    url 匹配，更复杂一些。

--- server-tornado-url.py
+++ server-tornado-url.py
@@ -6,14 +6,14 @@ from tornado.options import options
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('<a href="%s">link to story 1</a>' % self.reverse_url("story", "1"))
+        self.write('<a href="%s">link to story X</a>' % self.reverse_url("story", "1"))
 
 
 class StoryHandler(tornado.web.RequestHandler):
     def initialize(self, db):
         self.db = db
 
-    def get(self, story_id=None):
+    def get(self, story_id):
         self.write("this is story %s" % story_id)
 
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     options.parse_command_line()
     app = tornado.web.Application([
         tornado.web.url(r"/", MainHandler),
-        tornado.web.url(r"/story/([0-9]+)", StoryHandler, dict(db='mongo-x'), name="story")
-    ])
+        tornado.web.url(r"/story/?$|/story/([0-9]+)", StoryHandler, dict(db='mongo-x'), name="story")
+    ], debug=True)
     app.listen(8080)
     tornado.ioloop.IOLoop.current().start()
